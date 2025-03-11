f833086ee0fdf9b92206cdbb9b207f73c32a3156
Anuvind Pramod <99132098+anuvindpramod@users.noreply.github.com>
2025-03-11 12:46:43 +0530

    Update train.py

--- train.py
+++ train.py
@@ -120,7 +120,7 @@ def main():
     transforms.RandomResizedCrop((224,224)),
     transforms.RandomHorizontalFlip(),transforms.RandomVerticalFlip(),  
     transforms.RandomRotation(15),    
-    transforms.ColorJitter(brightness=0.1, contrast=0.1),  # Add brightness/contrast variation
+    transforms.ColorJitter(brightness=0.1, contrast=0.1),  # brightness/contrast variation
     transforms.RandomAffine(degrees=0, translate=(0.05, 0.05), scale=(0.95, 1.05)),
     transforms.ToTensor(),
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
