cbe1cfcea0f45ee2119eacef92881ec06285b014
Anuvind Pramod <99132098+anuvindpramod@users.noreply.github.com>
2025-03-11 12:48:44 +0530

    Update testing_scipt.py

--- testing_scipt.py
+++ testing_scipt.py
@@ -1,12 +1,1 @@
-import pandas as pd
-from dataset_2 import MammoDataset
-from config import Image_dir,CSV_dir
 
-# Quick validation test
-test_df = pd.read_csv(f'{CSV_dir}/mass_test_pro.csv').head(5)
-dataset = MammoDataset(test_df, None, Image_dir)
-
-print(f"Valid samples: {len(dataset)}")
-for i in range(3):
-    img, label = dataset[i]
-    print(f"Sample {i}: Shape {img.shape}, Label {label}")
